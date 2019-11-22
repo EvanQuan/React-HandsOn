8a5f69dd11ab2392391cf102ccd6214014517c88
Evan Quan <evanlquan@gmail.com>
2019-11-22 00:00:31 -0700

    Add SquareStyle

--- tic-tac-toe/src/Square.js
+++ tic-tac-toe/src/Square.js
@@ -1,40 +1,7 @@
 // @ts-check
 import React from 'react';
 import SquareState from './SquareState';
-
-/**
- * Default button style.
- */
-const buttonStyleDefault = {
-    width: '145px',
-    height: '145px',
-    backgroundColor: 'cadetblue',
-    color: 'cadetblue',
-    fontSize: '125px',
-    padding: '1px !important'
-}
-
-/**
- * Button style for the 'O' player.
- */
-const buttonStylePlayerO = {
-    width: '145px',
-    height: '145px',
-    backgroundColor: 'darkblue',
-    fontSize: '125px',
-    padding: '1px !important'
-}
-
-/**
- * Button style for the 'X' player.
- */
-const buttonStylePlayerX = {
-    width: '145px',
-    height: '145px',
-    backgroundColor: 'darkgray',
-    fontSize: '125px',
-    padding: '1px !important'
-}
+import SquareStyle from './SquareStyle';
 
 /**
  * A square of a grid in a Tic Tac Toe board.
@@ -48,9 +15,9 @@ class Square extends React.Component {
         return (
             <button
                 style={
-                    this.props.value === SquareState.PLAYER_X ? buttonStylePlayerX :
-                        this.props.value === SquareState.PLAYER_O ? buttonStylePlayerO :
-                            buttonStyleDefault}
+                    this.props.value === SquareState.PLAYER_X ? SquareStyle.PLAYER_X :
+                        this.props.value === SquareState.PLAYER_O ? SquareStyle.PLAYER_O :
+                            SquareStyle.DEFAULT}
                 onClick={this.props.onClick}
             >
                 {this.props.value}
---
+++ tic-tac-toe/src/SquareStyle.js
@@ -0,0 +1,40 @@
+/**
+ * {@link Square} styles.
+ */
+const SquareStyle = {
+    /**
+     * Default {@link Square} style.
+     */
+    DEFAULT : {
+        width: '145px',
+        height: '145px',
+        backgroundColor: 'cadetblue',
+        color: 'cadetblue',
+        fontSize: '125px',
+        padding: '1px !important'
+    },
+
+    /**
+     * Player 'X' {@link Square} style.
+     */
+    PLAYER_X : {
+        width: '145px',
+        height: '145px',
+        backgroundColor: 'darkgray',
+        fontSize: '125px',
+        padding: '1px !important'
+    },
+
+    /**
+     * Player 'O' {@link Square} style.
+     */
+    PLAYER_O : {
+        width: '145px',
+        height: '145px',
+        backgroundColor: 'darkblue',
+        fontSize: '125px',
+        padding: '1px !important'
+    },
+}
+
+export default SquareStyle;
